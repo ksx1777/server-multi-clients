5463f04b56dfe3dd7ef056d9906b780112d87350
ksx1777 <70083608+ksx1777@users.noreply.github.com>
2021-03-16 11:26:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,26 @@
 #include "server.h"
 
+//#define TIMEOUT_SEC 0x5
+
+#define RECV_LENGTH 1024 * 64
+
 /* variáveis globais */
 struct sockaddr_in *client_list;
-
 new_arg_list console_args[] = {
 	{"-ip", "endereço ip do servidor na rede."},
 	{"-port", "porta usada para iniciar conexões com o endereço ip."},
 	{"-queue", "número máximo de conexões simultâneas."}
 };
-
 new_socket_conf socket_conf[] = {0x0, 0x0, 0x0};
-
 int atual_conn = 0;
 int sofd;
 pthread_mutex_t queue_mutex;
 int *connected;
 
+/* funções */
 void help(void){
 	printf("[i] comandos disponíveis: \n");
-	for(int i = 0; i < 3; i ++){
-		printf(" %s : %s\n", console_args[i].name, console_args[i].help);
-	}
+	for(int i = 0; i < 3; i ++) printf(" %s : %s\n", console_args[i].name, console_args[i].help);
 }
 
 char *get_argument(int argc, char **arg_value, char *arg_name){
@@ -34,10 +34,9 @@ int socket_bind(char *ip, short port, int queue_length){
 	server_info.sin_addr.s_addr = inet_addr(ip);
 	server_info.sin_port = htons(port);
 	server_info.sin_family = AF_INET;
-
 	int s = socket(AF_INET, SOCK_STREAM, FALSE);
-	if(setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) != FALSE) err(EXIT_FAILURE, "");
-	else if(bind(s, (struct sockaddr*)&server_info, sizeof(server_info)) != FALSE) err(EXIT_FAILURE, "");
+	if(setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) != FALSE) err(EXIT_FAILURE, "\0");
+	else if(bind(s, (struct sockaddr*)&server_info, sizeof(server_info)) != FALSE) err(EXIT_FAILURE, "\0");
 	else if(listen(s, queue_length) != FALSE) err(EXIT_FAILURE, "[f]");
 	return s;
 }
@@ -45,7 +44,13 @@ int socket_bind(char *ip, short port, int queue_length){
 int init_new_conn(int s, int conn_num){
 	int conn_length = sizeof(struct sockaddr_in);
 	int socket_fd = accept(s, (struct sockaddr*)&client_list[conn_num], &conn_length);
-	if(socket_fd == -1) err(EXIT_FAILURE, "");
+	if(socket_fd == -1) err(EXIT_FAILURE, "\0");
+	
+/*	struct timeval tv;
+	tv.tv_sec = TIMEOUT_SEC;
+	tv.tv_usec = 0;
+	if(setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv) != FALSE) err(EXIT_FAILURE, "\0");
+*/
 	printf("[*] nova conexão aberta vinda do endereço %s:%i (%i)\n", inet_ntoa(client_list[conn_num].sin_addr), client_list[conn_num].sin_port, conn_num+1);
 	return socket_fd;
 }
@@ -55,15 +60,22 @@ int send_packet(int conn_num, char *content, int length){
 		printf("[*] cliente %s:%i (%i) desconectou-se.\n", inet_ntoa(client_list[conn_num].sin_addr), client_list[conn_num].sin_port, conn_num+1);
 		close(connected[conn_num]);
 		mset(&connected[conn_num], 0x0, sizeof(connected[conn_num]));
+		printf("[*] conexão fechada com o endereço %s:%i (%i)\n\n", inet_ntoa(client_list[conn_num].sin_addr), client_list[conn_num].sin_port, conn_num+1);
 		return -1;
 	}
 	return FALSE;
 }
 
-void *connections_handler(void *arg){
-	usleep((rand() % 100) * 1000);
-	int c = 0;	
+int recv_packet(int conn_num, char *store, int length){
+	return recv(connected[conn_num], store, length, MSG_DONTWAIT);
+}
 
+void *connections_handler(void *arg){
+	usleep((rand() % 50) * 1000);
+	int c = 0;
+	int bytes = 0;
+	char * packet_send = (char*)malloc(RECV_LENGTH * sizeof(char));
+	char * packet_recv = (char*)malloc(RECV_LENGTH * sizeof(char));
 	while(TRUE){
 		pthread_mutex_lock(&queue_mutex);
 		for(atual_conn = 0; atual_conn < socket_conf->max_conn;){
@@ -75,16 +87,23 @@ void *connections_handler(void *arg){
 		}
 		pthread_mutex_unlock(&queue_mutex);
 	
-
 		while(TRUE){
 			/* código que o servidor executará enquanto conectado com o cliente */
-			usleep(200 * 1000);
-			if(send_packet(c, "Teste\n", 6) == -1){
-				break;
+			usleep(10 * 1000);
+			mset(packet_recv, 0x0, sizeof(packet_recv));
+			mset(packet_send, 0x0, sizeof(packet_send));
+
+			/* enviar dados para o cliente */
+			if(send_packet(c, "Teste\n", 6) == -1) break;
+
+			/* recebimento de dados do cliente */
+			bytes = recv_packet(c, packet_recv, RECV_LENGTH);
+			if(bytes > 0){
+				printf("[*] %i bytes recebidos de endereço: %s:%i (%i)\n", bytes, inet_ntoa(client_list[c].sin_addr), client_list[c].sin_port, c+1);
+				/* etc... */
 			}
 		}
-			/* conexão se encerra no final do loop */
-		printf("[*] conexão fechada com o endereço %s:%i (%i)\n\n", inet_ntoa(client_list[c].sin_addr), client_list[c].sin_port, c+1);
+		/* final do loop while */
 	}
 	return NULL;
 }
@@ -110,21 +129,14 @@ int main(int argc, char** argv){
 	connected = (int*)calloc(socket_conf->max_conn, sizeof(int));
 
 	/* funções relacionadas ao gerenciamento do servidor */
-
-	if(pthread_mutex_init(&queue_mutex, NULL)) {
-    	err(EXIT_FAILURE, "%s", "Não foi possível iniciar a fila de threads.\n");
-	}
+	if(pthread_mutex_init(&queue_mutex, NULL)) err(EXIT_FAILURE, "%s", "Não foi possível iniciar a fila de threads.\n");
 
 	printf("[*] aguardando conexão no endereço %s:%hi ...\n", socket_conf->ip, socket_conf->port);
 	sofd = socket_bind(socket_conf->ip, socket_conf->port, socket_conf->max_conn);
+
 	pthread_t *threads = calloc(socket_conf->max_conn, sizeof(pthread_t));
 
-  	for(int i = 0; i < socket_conf->max_conn-1; i++) {
-    	if(pthread_create(&threads[i], NULL, connections_handler, NULL)) {
-      		err(EXIT_FAILURE, "%s", "Erro ao criar thread");
-		}
-	}
-
+  	for(int i = 0; i < socket_conf->max_conn-1; i++) if(pthread_create(&threads[i], NULL, connections_handler, NULL)) err(EXIT_FAILURE, "%s", "Erro ao criar thread");
 	connections_handler(NULL);
 	return TRUE;
 }
